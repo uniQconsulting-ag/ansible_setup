ae0c912d3cd4a975c126b93003510d3159ee51b7
Melvin Suter <44713851+melvin-suter@users.noreply.github.com>
2020-12-10 13:20:08 +0100

    Update install_0_prep_ansible.sh

--- install_0_prep_ansible.sh
+++ install_0_prep_ansible.sh
@@ -22,7 +22,7 @@ sed -i 's|^#nocows .*|nocows = 1|g' $ansibleconfigfile
 mkdir $PDIR || true
 chmod 700 $PDIR
 cd $PDIR
-git clone https://github.com/uniQconsulting-ag/ansible.os_basis_setup_project.git $PNAME
+git clone https://github.com/uniQconsulting-ag/ansible_setup.git $PNAME
 cd $PDIR/$PNAME
 test -d roles && rm -rf roles
 ansible-galaxy install uniqconsulting.open_vm_tools uniqconsulting.os_basic uniqconsulting.firewall
